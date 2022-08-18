af392f44d2187165c97831270cff298d3784745f
Isabel Garrido <isabel.garrido@code.seat>
2022-08-18 18:06:27 +0200

    Update structure lesson 02-01

--- 02-01-create-course/src/main/kotlin/com/codely/course/application/CourseCreator.kt
+++ 02-01-create-course/src/main/kotlin/com/codely/course/application/CourseCreator.kt
@@ -1,7 +1,7 @@
-package com.codely.api.application
+package com.codely.course.application
 
-import com.codely.api.domain.course.Course
-import com.codely.api.domain.course.CourseRepository
+import com.codely.course.domain.course.Course
+import com.codely.course.domain.course.CourseRepository
 import java.time.LocalDateTime
 import java.util.UUID
 
--- 02-01-create-course/src/main/kotlin/com/codely/course/domain/course/Course.kt
+++ 02-01-create-course/src/main/kotlin/com/codely/course/domain/course/Course.kt
@@ -1,4 +1,4 @@
-package com.codely.api.domain.course
+package com.codely.course.domain.course
 
 import java.time.LocalDateTime
 import java.util.UUID
