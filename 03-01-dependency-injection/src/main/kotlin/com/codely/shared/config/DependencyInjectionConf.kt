406488d5eaea845fd43fc480fdac0cb6ee9979ee
Isabel Garrido <isabel.garrido@code.seat>
2022-08-20 21:00:35 +0200

    Add dependency injection with @Bean and @Configuration

---
+++ 03-01-dependency-injection/src/main/kotlin/com/codely/shared/config/DependencyInjectionConf.kt
@@ -0,0 +1,16 @@
+package com.codely.shared.config
+
+import com.codely.course.application.CourseCreator
+import com.codely.course.infrastructure.persistence.InMemoryCourseRepository
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class DependencyInjectionConf {
+
+    @Bean
+    fun courseRepository() = InMemoryCourseRepository()
+
+    @Bean
+    fun courseCreator() = CourseCreator(courseRepository())
+}
