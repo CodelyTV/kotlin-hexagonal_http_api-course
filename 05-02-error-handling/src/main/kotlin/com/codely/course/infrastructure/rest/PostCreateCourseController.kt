b6dbe6dc128034c62aadae031afcb3a827010e6c
Isabel Garrido <isabel.garrido@code.seat>
2022-08-21 21:42:59 +0200

    Handle errors

--- 05-02-error-handling/src/main/kotlin/com/codely/course/infrastructure/rest/PostCreateCourseController.kt
+++ 05-02-error-handling/src/main/kotlin/com/codely/course/infrastructure/rest/PostCreateCourseController.kt
@@ -1,6 +1,10 @@
 package com.codely.course.infrastructure.rest
 
 import com.codely.course.application.CourseCreator
+import com.codely.course.domain.course.InvalidCourseIdException
+import com.codely.course.domain.course.InvalidCourseNameException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -11,8 +15,22 @@ class PostCreateCourseController(private val courseCreator: CourseCreator) {
     @PostMapping("/course")
     fun execute(
         @RequestBody request: CreateCourseRequest
-    ) {
-        courseCreator.create(request.id, request.name)
+    ): ResponseEntity<String> {
+        return try {
+            courseCreator.create(request.id, request.name)
+            ResponseEntity.ok().body("")
+        } catch (exception: Throwable) {
+            when (exception) {
+                is InvalidCourseIdException,
+                is InvalidCourseNameException -> ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(exception.message)
+
+                else -> ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Something went bad")
+            }
+        }
     }
 }
 
