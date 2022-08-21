790e11b9fc12763c1c6b1447710781284befa867
Isabel Garrido <isamadrid90@users.noreply.github.com>
2022-08-21 18:57:03 +0200

    Add lesson 04 (#8)

---
+++ 04-01-add-repo/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "04-01-add-repo"
---
+++ 04-01-add-repo/src/main/kotlin/com/codely/course/infrastructure/persistence/PostgreCourseRepository.kt
@@ -0,0 +1,22 @@
+package com.codely.course.infrastructure.persistence
+
+import com.codely.course.domain.course.Course
+import com.codely.course.domain.course.CourseRepository
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+
+class PostgreCourseRepository(private val jdbcTemplate: NamedParameterJdbcTemplate) : CourseRepository {
+
+    override fun save(course: Course) {
+        MapSqlParameterSource()
+            .addValue("id", course.id.value)
+            .addValue("name", course.name.value)
+            .addValue("createdAt", course.createdAt)
+            .let { params ->
+                jdbcTemplate.update(
+                "INSERT INTO course (id, name, created_at) VALUES (:id,:name,:createdAt)",
+                    params
+                )
+            }
+    }
+}
