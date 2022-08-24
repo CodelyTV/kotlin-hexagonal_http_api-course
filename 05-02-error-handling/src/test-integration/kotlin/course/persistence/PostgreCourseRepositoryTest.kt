719f75482317cf42e182fa5c30b2004c4d189d09
Isabel Garrido <isabel.garrido@code.seat>
2022-08-24 20:45:50 +0200

    Remove h2 use testcontainer

--- 05-02-error-handling/src/test-integration/kotlin/course/persistence/PostgreCourseRepositoryTest.kt
+++ 05-02-error-handling/src/test-integration/kotlin/course/persistence/PostgreCourseRepositoryTest.kt
@@ -15,14 +15,13 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.core.RowMapper
-import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
+import shared.persistence.BaseIntegrationTest
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ContextConfiguration(classes = [Application::class])
-@ActiveProfiles("test")
-class PostgreCourseRepositoryTest {
+class PostgreCourseRepositoryTest : BaseIntegrationTest() {
 
     @Autowired
     private lateinit var repository: PostgreCourseRepository
