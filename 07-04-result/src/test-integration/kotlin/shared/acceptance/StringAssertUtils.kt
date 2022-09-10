ccc77750586fb2b8ee5c9364c99f5ac48afe16e9
Isabel Garrido <isabel.garrido@code.seat>
2022-09-10 12:59:38 +0200

    Move result to new folder

---
+++ 07-04-result/src/test-integration/kotlin/shared/acceptance/StringAssertUtils.kt
@@ -0,0 +1,9 @@
+package shared.acceptance
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import kotlin.test.assertEquals
+
+fun String.isEqualToJson(expected: String) {
+    val objectMapper = jacksonObjectMapper()
+    assertEquals(objectMapper.readTree(expected), objectMapper.readTree(this))
+}
