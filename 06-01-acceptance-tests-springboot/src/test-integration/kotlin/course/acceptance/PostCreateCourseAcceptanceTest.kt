16a893ad49a8053ec482ee8fe906f8108c5fba9d
Isabel Garrido <isabel.garrido@code.seat>
2022-09-11 13:16:14 +0200

    Simplify test and apply linter

--- 06-01-acceptance-tests-springboot/src/test-integration/kotlin/course/acceptance/PostCreateCourseAcceptanceTest.kt
+++ 06-01-acceptance-tests-springboot/src/test-integration/kotlin/course/acceptance/PostCreateCourseAcceptanceTest.kt
@@ -1,21 +1,14 @@
 package course.acceptance
 
-import com.codely.course.domain.course.Course
-import com.codely.course.domain.course.CourseId
-import com.codely.course.domain.course.CourseName
-import java.sql.ResultSet
-import java.time.LocalDateTime
-import java.util.UUID
-import kotlin.test.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.jdbc.core.JdbcTemplate
-import org.springframework.jdbc.core.RowMapper
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import shared.acceptance.BaseAcceptanceTest
+import kotlin.test.assertEquals
 
 class PostCreateCourseAcceptanceTest : BaseAcceptanceTest() {
 
@@ -42,28 +35,5 @@ class PostCreateCourseAcceptanceTest : BaseAcceptanceTest() {
             .andExpect {
                 assertEquals("", it.response.contentAsString)
             }
-
-        val courseFromDb = jdbcTemplate.queryForObject(
-            "select * from course where id=?",
-            mapRow(),
-            "97fa5af4-bd81-45d5-974f-d5a3970af252"
-        )
-
-        assertEquals(
-            Course(
-                CourseId.fromString("97fa5af4-bd81-45d5-974f-d5a3970af252"),
-                CourseName("Test Acceptance"),
-                LocalDateTime.now()
-            ), courseFromDb
-        )
-    }
-
-    private fun mapRow(): RowMapper<Course> {
-        return RowMapper { rs: ResultSet, _: Int ->
-            val id = CourseId(UUID.fromString(rs.getString("id")))
-            val name = CourseName(rs.getString("name"))
-            val createdAt = rs.getTimestamp("created_at").toLocalDateTime()
-            Course(id, name, createdAt)
-        }
     }
 }
