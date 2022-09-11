9037d563611cc6d676bb653360a67e5b50e2b2ae
Isabel Garrido <isabel.garrido@code.seat>
2022-09-11 16:57:43 +0200

    Alternative structure

---
+++ 02-01-create-course-alternative-structure/contexts/course/src/main/kotlin/com/codely/domain/CourseRepository.kt
@@ -0,0 +1,5 @@
+package com.codely.domain
+
+interface CourseRepository {
+    fun save(course: Course)
+}
---
+++ 02-01-create-course-alternative-structure/settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "02-01-create-course-alternative-structure"
+include("contexts:course")
