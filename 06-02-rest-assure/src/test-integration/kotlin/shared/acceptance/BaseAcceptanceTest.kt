4b888f79d067347499f55533d4b2a6d536f4c624
Isabel Garrido <isabel.garrido@code.seat>
2022-09-11 13:22:34 +0200

    Apply linter 06-02

--- 06-02-rest-assure/src/test-integration/kotlin/shared/acceptance/BaseAcceptanceTest.kt
+++ 06-02-rest-assure/src/test-integration/kotlin/shared/acceptance/BaseAcceptanceTest.kt
@@ -3,6 +3,7 @@ package shared.acceptance
 import com.codely.shared.Application
 import io.mockk.unmockkAll
 import io.restassured.RestAssured
+import java.io.File
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeAll
@@ -14,7 +15,6 @@ import org.springframework.test.context.ActiveProfiles
 import org.testcontainers.containers.DockerComposeContainer
 import org.testcontainers.containers.wait.strategy.Wait
 import org.testcontainers.junit.jupiter.Testcontainers
-import java.io.File
 
 @SpringBootTest(classes = [Application::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
