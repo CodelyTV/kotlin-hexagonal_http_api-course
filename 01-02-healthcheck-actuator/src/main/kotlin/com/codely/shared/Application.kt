f3fbcc52634beccbea1958402ed16f9c4069a5eb
Isabel Garrido <isabel.garrido@code.seat>
2022-10-24 15:08:51 +0200

    Update 1

--- 01-02-healthcheck-actuator/src/main/kotlin/com/codely/shared/Application.kt
+++ 01-02-healthcheck-actuator/src/main/kotlin/com/codely/shared/Application.kt
@@ -2,7 +2,6 @@ package com.codely.shared
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
 class Application
