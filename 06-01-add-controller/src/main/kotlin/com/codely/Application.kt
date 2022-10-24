a0d1cf8905e5542c44ad15edeabe47b70f8f3548
Isabel Garrido <isabel.garrido@code.seat>
2022-10-24 11:30:50 +0200

    Fix tests

--- 06-01-add-controller/src/main/kotlin/com/codely/Application.kt
+++ 06-01-add-controller/src/main/kotlin/com/codely/Application.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan("com.codely")
+@ComponentScan("com.codely.*")
 class Application
 
 fun main(args: Array<String>) {
